71aa4ba391e2b5b684c532ebc167e75e3f2be913
Kota Cody Enokida <codyenokida@gmail.com>
2024-06-23 17:52:07 -0700

    Added some content to the comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,15 +1,80 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 import HandWaveIcon from "@/public/hand-waving.svg";
 import ExitIcon from "@/public/exit.svg";
 
 import styles from "./Comments.module.scss";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Comments() {
+  const commentRef = useRef<HTMLDivElement>(null);
+
   const [commentsOpen, setCommentsOpen] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  /**
+   * Comment state
+   */
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [name, setName] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  /**
+   * Check if mobile
+   */
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  /**
+   * Re-style html and body so they are not scrollable
+   */
+  useEffect(() => {
+    if (commentsOpen && isMobile) {
+      const html = document.querySelector("html");
+
+      document.body.style.overflow = "hidden";
+      document.body.style.touchAction = "none";
+
+      if (html && html instanceof HTMLElement) {
+        html.style.overflow = "hidden";
+        html.style.touchAction = "none";
+      }
+
+      // Cleanup function to remove the style when component unmounts
+      return () => {
+        document.body.style.overflow = "auto";
+        document.body.style.touchAction = "auto";
+
+        if (html && html instanceof HTMLElement) {
+          html.style.overflow = "auto";
+          html.style.touchAction = "auto";
+        }
+      };
+    }
+  }, [commentsOpen]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (commentRef.current && !commentRef.current.contains(event.target)) {
+        setCommentsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [setCommentsOpen]);
 
   return (
     <div className={styles.container}>
@@ -34,7 +99,14 @@ export default function Comments() {
         </motion.div>
       </motion.button>
       <motion.div
-        initial={{ right: 0, bottom: -1000, opacity: 0, display: "none" }}
+        ref={commentRef}
+        initial={{
+          right: 0,
+          bottom: 64,
+          opacity: 0,
+          display: "none",
+          height: 38,
+        }}
         animate={
           commentsOpen
             ? {
@@ -42,106 +114,82 @@ export default function Comments() {
                 bottom: 64,
                 opacity: 1,
                 display: "flex",
-                rotate: [-45, -15, 0],
+                height: [38, isMobile ? 540 : 600],
               }
-            : { right: 16, bottom: -1000, opacity: 0 }
+            : {
+                right: 16,
+                bottom: 64,
+                opacity: 0,
+                display: "none",
+                height: [isMobile ? 540 : 600, 38],
+                width: 0,
+              }
         }
-        transition={{ ease: "easeInOut", duration: 0.45 }}
+        transition={{
+          ease: "backInOut",
+          duration: 0.6,
+          damping: 10,
+          spring: 1,
+        }}
         className={styles.comments}
       >
-        Feel free to leave any thoughts!
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br /> <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        hi
+        {commentsOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={
+              commentsOpen
+                ? {
+                    opacity: 1,
+                    y: [8, 0],
+                  }
+                : {
+                    opacity: 0,
+                  }
+            }
+            transition={{
+              ease: "backInOut",
+              duration: 0.4,
+              delay: 0.5,
+            }}
+            className={styles.content}
+          >
+            <h2>Leave any thoughts or comments here!</h2>
+            {comments.length === 0 && <p>No comments, yet... chime in!</p>}
+            <div className={styles.inputContainer}>
+              <label>Name</label>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={styles.nameInput}
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label>Content</label>
+              <textarea
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className={styles.contentInput}
+              />
+            </div>
+          </motion.div>
+        )}
       </motion.div>
+      <AnimatePresence>
+        {isMobile && commentsOpen && (
+          <motion.div
+            animate={{
+              opacity: [0, 1],
+            }}
+            transition={{
+              ease: "backInOut",
+              duration: 0.6,
+              damping: 10,
+              spring: 1,
+            }}
+            className={styles.modal}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
